886a73537fe64ebd0c8f651fca41bbfecb0de5ba
David Andersson <kallkorvhd@gmail.com>
2024-10-10 13:56:23 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public class Circle
+    {
+       
+        public int Radie { get; set; }
+        public Circle(int radie)
+        {
+            Radie = radie; 
+        }
+
+        public double GetArea()
+        {
+            return Radie * Radie * Math.PI;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(4);
+
+            Console.WriteLine(circle.GetArea()); 
+        }
+    }
+}
